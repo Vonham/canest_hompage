6fe8d5cde76fd82a0a4d9ad0aebf8dc849b7976e
SungminCho <cho@delvine.co.kr>
2025-09-24 20:48:00 +0900

    배포포

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command }) => {
-  const base = command === 'serve' ? '/' : '/your-repo-name/';
+  const base = command === 'serve' ? '/' : '/canest_hompage/';
   return {
     base,
     plugins: [react()],
