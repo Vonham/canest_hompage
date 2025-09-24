25c352e6dbe6b7366c14c3a4edb6799ff229506e
SungminCho <cho@delvine.co.kr>
2025-09-24 21:01:44 +0900

    Chore: 개인 도메인 연결에 따른 base 경로 수정

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command }) => {
-  const base = command === 'serve' ? '/' : '/canest_hompage/';
-  return {
-    base,
-    plugins: [react()],
-  }
+export default defineConfig({
+  base: '/',
+  plugins: [react()],
 })
